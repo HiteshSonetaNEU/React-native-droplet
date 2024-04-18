156d63ac610957e0cbf1f69b8a3eaa98f6ddea0d
HiteshSonetaNEU <144804156+HiteshSonetaNEU@users.noreply.github.com>
2024-04-17 20:05:26 -0400

    removed unwanted component

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import TaskList from './components/TaskList';
-import TaskDetails from './components/TaskDetails';
 import NewTask from './components/NewTask';
 import EditTask from './components/EditTask';
 
@@ -13,7 +12,6 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="TaskList">
         <Stack.Screen name="TaskList" component={TaskList} options={{ title: 'Task List' }} />
-        <Stack.Screen name="TaskDetails" component={TaskDetails} options={{ title: 'Task Details' }} />
         <Stack.Screen name="NewTask" component={NewTask} options={{ title: 'New Task' }} />
         <Stack.Screen name="EditTask" component={EditTask} options={{ title: 'Edit Task' }} />
       </Stack.Navigator>
